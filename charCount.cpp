46e13674e8016ede065c47e461b5a4562e200a1f
charlesdominique <charlesdominiquebacanto@gmail.com>
2024-10-21 17:47:13 +0800

    comments

--- charCount.cpp
+++ charCount.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
-
+//Counting characters in a string activity
 int main(){
     std:: string stringInput;
     char characterInput;
     int charcount;
 
+    //Prompts the user to enter a word
     std::cout << "Please enter a word or phrase: " << std:: endl;
+    //Stores the input word
     std:: getline(std::cin, stringInput);
+    //Prompts the user to enter a character to count
     std::cout << "Please enter a character to count: " << std:: endl;
+     //Stores the input letter/character
     std:: cin >> characterInput;
 
     for(char c : stringInput){
